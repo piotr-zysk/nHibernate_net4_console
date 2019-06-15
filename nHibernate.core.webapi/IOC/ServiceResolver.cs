e58e778676347566c9dcf05faf8b2104c0e50cf7
Piotr Zysk <pizy123@gmail.com>
2019-06-15 11:57:36 +0200

    Castle.Windsor installed and used for resolving dependencies in web api project.
    Resolving NHibernate, IUserRepository via Castle.Windsor

---
+++ nHibernate.core.webapi/IOC/ServiceResolver.cs
@@ -0,0 +1,42 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.MsDependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using nHibernate.core.webapi.Repositories;
+using NHibernate.Cfg;
+using System;
+using System.Reflection;
+
+namespace nHibernate.core.webapi.IOC
+{
+    public class ServiceResolver
+    {
+        private static WindsorContainer container;
+        private static IServiceProvider serviceProvider;
+
+        public ServiceResolver(IServiceCollection services)
+        {
+            container = new WindsorContainer();
+
+            container.Register(Classes.FromAssembly(Assembly.GetExecutingAssembly())
+                .BasedOn<IUserRepository>().WithService.FromInterface());
+
+            
+            container.Register(Component
+                .For<NHibernate.ISessionFactory>()
+                .Instance(new Configuration().Configure().BuildSessionFactory()));
+
+            container.Register(Component
+                .For<NHibernate.ISession>()
+                .UsingFactoryMethod(kernel => kernel.Resolve<NHibernate.ISessionFactory>().OpenSession())
+                .LifeStyle.Transient);
+                       
+            serviceProvider = WindsorRegistrationHelper.CreateServiceProvider(container, services);
+        }
+
+        public IServiceProvider GetServiceProvider()
+        {
+            return serviceProvider;
+        }
+    }
+}
