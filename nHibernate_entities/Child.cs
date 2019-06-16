6dc3192a9bd92d9388dd562983648a8c1cb526e1
Piotr Zysk <pizy123@gmail.com>
2019-06-16 09:25:09 +0200

    Added models and mapping for Parent/Child.

---
+++ nHibernate_entities/Child.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nHibernate.entities
+{
+    public class Child
+    {
+        public virtual int Id { get; set; }
+        public virtual int Name { get; set; }
+        public virtual Parent Parent { get; set; }
+
+    }
+}
---
+++ nHibernate_entities/Parent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nHibernate.entities
+{
+    public class Parent
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual ISet<Child> Children { get; set; }
+    }
+}
